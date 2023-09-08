175092b5ec5a801e2a4de0c39946e59b602e72ea
Cameron VanderTuig <camvandertuig@gmail.com>
2023-09-08 14:14:16 -0700

    Work on commands. Need to redo channel info getting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use ascii::AsAsciiStr;
 use clap::{arg, Parser, Subcommand};
 use dante_control_rs::{print_arc, print_chan, print_cmc, print_dbc, DanteDeviceManager};
 use std::thread::sleep;
@@ -21,11 +22,11 @@ enum Commands {
     /// Lists the available dante devices on the local network
     ListDevices {
         /// Seconds to wait for mDNS to resolve before printing discovered devices
-        #[arg(default_value_t = 2.0, short, long)]
+        #[arg(default_value_t = 5.0, short, long)]
         time: f32,
 
         /// Print detailed info instead of just device names.
-        #[arg(default_value_t = false, short, long)]
+        #[arg(short, long)]
         detailed: bool,
     },
 
@@ -40,6 +41,39 @@ enum Commands {
         detailed: bool,
     },
 
+    /// Command for controlling dante devices.
+    #[command(subcommand)]
+    Control(ControlCommands),
+
+    /// Debug commands (mostly for mDNS).
+    #[command(subcommand)]
+    Debug(DebugCommands),
+}
+
+#[derive(Subcommand, Debug)]
+enum ControlCommands {
+    /// Make subscription
+    MakeSubscription {
+        /// Name of the dante device to transmit the new subscription
+        transmitter_name: String,
+
+        /// Channel id of the dante device to transmit the new subscription
+        transmitter_channel_id: u16,
+
+        /// Name of the dante device to receive the new subscription
+        receiver_name: String,
+
+        /// Channel id of the dante device to receive the new subscription
+        receiver_channel_id: u16,
+
+        /// Seconds to wait for mDNS to resolve before printing discovered devices
+        #[arg(default_value_t = 5.0, short, long)]
+        time: f32,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+enum DebugCommands {
     /// Lists information about mDNS discovery on the "_netaudio-cmc._udp.local." address.
     PrintCMC {
         /// Seconds to wait for mDNS to resolve
@@ -134,21 +168,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
         }
-        Some(Commands::PrintCMC { time }) => {
-            print_cmc(Duration::from_secs_f32(*time));
-        }
-        Some(Commands::PrintDBC { time }) => {
-            print_dbc(Duration::from_secs_f32(*time));
-        }
-        Some(Commands::PrintARC { time }) => {
-            print_arc(Duration::from_secs_f32(*time));
-        }
-        Some(Commands::PrintCHAN { time }) => {
-            print_chan(Duration::from_secs_f32(*time));
-        }
+        Some(Commands::Debug(debug_command)) => match debug_command {
+            DebugCommands::PrintCMC { time } => {
+                print_cmc(Duration::from_secs_f32(*time));
+            }
+            DebugCommands::PrintDBC { time } => {
+                print_dbc(Duration::from_secs_f32(*time));
+            }
+            DebugCommands::PrintARC { time } => {
+                print_arc(Duration::from_secs_f32(*time));
+            }
+            DebugCommands::PrintCHAN { time } => {
+                print_chan(Duration::from_secs_f32(*time));
+            }
+        },
         None => {
             println!("No command specified. Try \"dante-cli help\"");
         }
+        Some(Commands::Control(control_command)) => match control_command {
+            ControlCommands::MakeSubscription {
+                transmitter_name,
+                transmitter_channel_id,
+                receiver_name,
+                receiver_channel_id,
+                time,
+            } => {
+                let receiver_name_ascii = receiver_name.as_ascii_str()?;
+                let transmitter_name_ascii = transmitter_name.as_ascii_str()?;
+
+                let mut device_manager = DanteDeviceManager::new();
+                device_manager.start_discovery()?;
+
+                if !args.quiet {
+                    println!("Discovering Devices...");
+                }
+
+                sleep(Duration::from_secs_f32(*time));
+
+                device_manager.stop_discovery();
+
+                device_manager.make_subscription(
+                    receiver_name_ascii,
+                    *receiver_channel_id,
+                    transmitter_name_ascii,
+                    *transmitter_channel_id,
+                )?;
+            }
+        },
     }
 
     Ok(())
